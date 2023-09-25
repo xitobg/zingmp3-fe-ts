7b37e2b7996c2589163579f9e51adc1f704b383b
xitobg <67237256+xitobg@users.noreply.github.com>
2023-09-25 17:26:38 +0700

    Update request.ts

--- src/helpers/request.ts
+++ src/helpers/request.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const request = axios.create({
-  baseURL: 'https://musictes.vercel.app/api',
+  baseURL: 'https://api-zingmp3-public.vercel.app/api',
 })
 
 request.interceptors.response.use(
